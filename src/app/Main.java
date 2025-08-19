b1d3b134b87a11f9a54f41eb8205ab5740922451
machine2111 <136928648+machine2111@users.noreply.github.com>
2025-08-18 20:38:26 -0400

    MAIN

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,9 +7,14 @@ import java.sql.SQLException;
 import dao.ReservasDAO;
 import model.Venta;
 
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+	static Scanner sc = new Scanner(System.in);
+	static boolean continuarMain = true;
+	
     public static void main(String[] args) {
         try (Connection conn = DBManager.getConnection()) {
             System.out.println("✅ Conexión exitosa a la base de datos!");
@@ -21,10 +26,99 @@ public class Main {
         }
         
         pruebaDAO();
+        
+        
+        mostrarBienvenida();
+
+		do {
+			mostrarMenu();
+			int opcionMenu = elegirOpcionMenu();
+			ejecutarOpcion(opcionMenu);
+
+		} while (continuarMain);
     }
     
+    public static void mostrarBienvenida() {
+		System.out.println("=".repeat(50));
+		System.out.println("-".repeat(6)
+				+ " BIENVENIDO AL SISTEMA DE GESTIÓN DE RESERVAS DE SALAS "
+				+ "-".repeat(6));
+		System.out.println("=".repeat(50));
+	}
     
-    public static void pruebaDAO() {
+	public static void mostrarMenu() {
+		System.out.println("\n" + "-".repeat(22) + " MENÚ " + "-".repeat(22));
+		System.out.println("(1) Registrar sala");
+		System.out.println("(2) Registrar usuario");
+		System.out.println("(3) Hacer reserva");
+		System.out.println("(4) Ver reservas");
+		System.out.println("(0) Salir");
+
+	}
+	
+	public static int elegirOpcionMenu() {
+		try {
+			System.out
+					.println("\nIngrese el número de la opción seleccionada: ");
+			int opcionSeleccionada = sc.nextInt();
+			sc.nextLine();
+			return opcionSeleccionada;
+		} catch (InputMismatchException e) {
+			System.out.println("⚠️ Entrada inválida. Debe ingresar un número.");
+			sc.nextLine();
+			return 12749;
+		}
+
+	}
+	
+	public static void ejecutarOpcion(int opcion) {
+		switch (opcion) {
+		case 1: // RESERVAR SALA
+			resgitrarSala();
+			break;
+		case 2: // REGISTRAR USUARIO
+			registrarUsuario();
+			break;
+		case 3: // HACER RESERVA
+			hacerReserva();
+			break;
+		case 4: // VER RESERVAS
+			verReservas();
+			break;
+		case 0: // SALIR DEL PROGRAMA
+			continuarMain = false;
+			System.out.printf("El programa ha finalizado.\n");
+			break;
+		case 12749: // NO INGRESO UN NÚMERO (YA SALIO EL MENSAJE)
+			break;
+		default: // ÓPCION INGRESADA NO ES DE 1 A 9
+			System.out.printf("⚠️ Opción no válida, intente nuevamente.\n");
+			break;
+		}
+
+	}
+    
+    private static void verReservas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void hacerReserva() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void registrarUsuario() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void resgitrarSala() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void pruebaDAO() {
     	
     	 ReservasDAO dao = new ReservasDAO();
 
